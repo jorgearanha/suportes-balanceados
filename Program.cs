7cfd65ec2171ce1245ccc9aa9b24a2ce03cf8b07
Jorge Luís Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2022-09-25 20:47:10 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SuportesBalanceados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+                Console.WriteLine("A entrada" + (isValid(arg) ? "" : " não") + " é um suporte balanceado.");
+            }
+        }
+
+        static Boolean isValid(string text)
+        {
+            Dictionary<char, char> closeToOpen = new Dictionary<char, char>(){
+                {']', '['},
+                {'}', '{'},
+                {')', '('}
+            };
+
+            char[] validOpen = new char[] { '{', '[', '(' };
+            char[] validClose = new char[] { '}', ']', ')' };
+
+            Stack<char> stack = new Stack<char>();
+            foreach (var word in text.ToCharArray())
+            {
+                if (validClose.Contains(word) && stack.Count > 0)
+                {
+                    char temp = stack.Pop();
+
+                    if (!temp.Equals(closeToOpen[word]))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(word);
+                }
+            }
+
+            return stack.Count <= 0;
+        }
+    }
+}
